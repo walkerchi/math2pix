c25a151d637edb6e3cc1a4034264113b845bdfec
walkerchi <walkerchi@foxmail.com>
2023-04-25 00:47:41 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useEffect, useState } from 'react';
+import styles from './App.module.css';
+import {remark} from 'remark'
+import remarkMath from 'remark-math'
+import remarkRehype from 'remark-rehype'
+import rehypeKatex from 'rehype-katex'
+import rehypeStringify from 'rehype-stringify'
+import {toSvg, toPng, toJpeg, toBlob} from 'html-to-image'
+import hightlight from 'remark-highlight.js'
+
+
+
+import "katex/dist/katex.min.css";
+
+
 
 function App() {
+  const [tex, setTex] = useState('x^2');
+  const [html, setHTML] = useState(null);
+  const [imgType, setImgType] = useState('png');
+
+
+  useEffect(()=>{
+
+    console.log(tex)
+    const processor = remark()
+      .use(remarkMath)
+      .use(remarkRehype)
+      .use(rehypeKatex)
+      .use(rehypeStringify)
+    const result = processor.processSync("$$\n" + tex + "\n$$")
+   
+    setHTML(result.value)
+    // update image
+
+   
+   
+  },[tex,imgType])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <h1>Math 2 Pix</h1>
+        <div className={styles.body}>
+        <textarea id="math" className={styles.math} 
+          onChange={e=>{
+          console.log(e.target.value)
+          setTex(e.target.value)}}
+          value={tex}
+          >
+        </textarea>
+        <div id="image" className={styles.image}>
+          <div id="math-image" className={styles["math-image"]} dangerouslySetInnerHTML={{__html:html}}></div>
+          <select onChange={e=>setImgType(e.target.value)}>
+            <option>png</option>
+            <option>jpg</option>
+            <option>svg</option>
+          </select>
+          <div className={styles.button}
+            onClick={e=>{
+
+              const htmlToImage = {
+                png:toPng,
+                jpg:toJpeg,
+                svg:toSvg
+              }[imgType]
+              htmlToImage(document.getElementById("math-image"))
+              .then((dataURL=>{
+                console.log(dataURL)
+                const link = document.createElement('a')
+                link.download = `math.${imgType}`
+                link.href = dataURL
+                document.body.appendChild(link)
+                link.click()
+                document.body.removeChild(link)
+                
+              }))
+              .catch(function (error) {
+                console.error('Unable to render image', error);
+              });
+             
+            }}
+          >Download</div>
+          
+        </div>
+        </div>
     </div>
   );
 }
